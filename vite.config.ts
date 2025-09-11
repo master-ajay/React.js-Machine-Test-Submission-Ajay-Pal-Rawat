601998ca2939b82f5da158c65a2ab0b519690443
master-ajay <palrawatajay@gmail.com>
2025-09-11 07:48:39 +0530

    create vite app and install dependencies

---
+++ vite.config.ts
@@ -0,0 +1,11 @@
+/// <reference types="vitest" />
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    globals: true,
+    environment: "node", // we don't need jsdom for pure unit tests
+  },
+});
